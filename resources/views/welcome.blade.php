68531774a78ee50c7fac1f0325a5d0d7c6893cca
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2024-05-03 17:56:54 +0300

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,70 +2,91 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Easy Api</title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&family=Inter:wght@100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
 </head>
+<style>
+    body {
+        color: #01415B;
+        font-family: "Montserrat", sans-serif;
+
+    }
+</style>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="#">Easy Api</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Google Api</a>
-                    </li>
-
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <div class="container my-5">
-
-        <div class="d-flex align-items-center gap-5">
-            <h1 class="my-2">Easy Api - Google Api </h1>
-            @if (session('success'))
-                <div id="successMessage" class="text-white border p-2 bg-success">
-                    {{ session('success') }}
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light shadow-sm"
+            style="background-color: #e3f2fd !important; color: #01415B !important;">
+            <div class="container">
+                <a class="navbar-brand" href="#" style=" color: #01415B !important; font-weight: bold;">Easy
+                    Api</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Google Api</a>
+                        </li>
+                    </ul>
                 </div>
-            @endif
-
-            @if (session('error'))
-                <div id="errorMessage" class="text-white border p-2 bg-danger">
-                    {{ session('error') }}
-                </div>
-            @endif
-        </div>
-
-        <form method="POST" action="{{ route('api.google_map_places.store-branches') }}">
-            @method('post')
-            @csrf
-            <label for="exampleFormControlSelect1" class="form-label">Select Category:</label>
-            <select class="form-select" id="exampleFormControlSelect1" onchange="fetchPlaces(this.value)"
-                name="places[]">
-                <option value="">Select Category</option>
-                @foreach (\App\Models\GoogleMapPlaces::all() as $place)
-                    <option value="{{ $place->id }}">{{ $place->keyword }}</option>
-                @endforeach
-            </select>
-
-            <div id="placesCheckboxes" class="mt-3">
             </div>
-            <button class="btn btn-primary mt-3">Submit</button>
-        </form>
-    </div>
+        </nav>
+    </header>
 
-    
+    <main
+        style="
+        min-height: 50vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: calc(100vh - 112px);
+      ">
+        <div class="container p-lg-5 p-3 shadow-sm rounded">
+            <div class="d-flex align-items-center gap-5">
+                <h1 class="my-2" style="font-size: 30px;">Easy Api - Google Api</h1>
+                @if (session('success'))
+                    <div id="successMessage" class="text-white border p-2 bg-success">
+                        {{ session('success') }}
+                    </div>
+                    @endif @if (session('error'))
+                        <div id="errorMessage" class="text-white border p-2 bg-danger">
+                            {{ session('error') }}
+                        </div>
+                    @endif
+            </div>
 
-    <footer class="bg-dark text-light text-center py-3 fixed-bottom">
+            <form method="POST" method="POST" action="{{ route('api.google_map_places.store-branches') }}">
+                @method('post') @csrf
+                <label for="exampleFormControlSelect1" class="form-label">Select Place:</label>
+                <select class="form-select" id="exampleFormControlSelect1" onchange="fetchPlaces(this.value)"
+                    name="places[]">
+                    <option value="">Select Place</option>
+                    @foreach (\App\Models\GoogleMapPlaces::all() as $place)
+                        <option value="{{ $place->id }}">{{ $place->keyword }}</option>
+                    @endforeach
+                </select>
+
+                <div id="placesCheckboxes" class="mt-3">
+                    
+                    
+                </div>
+                <button class="btn btn-primary mt-3"
+                    style="min-width: 150px; height: 47px;background-color: #01415B; border-color: #01415B;">Submit</button>
+            </form>
+        </div>
+    </main>
+
+    <footer class=" text-center py-3 fixed-bottom shadow-sm" style="background-color: #e3f2fd ; color: #01415B;">
         <div class="container">
             &copy; {{ Carbon\Carbon::parse(now())->format('Y') }} Easy Api
         </div>
@@ -80,24 +101,25 @@
     <script>
         function fetchPlaces(placeKeyWord) {
             if (placeKeyWord) {
-                const apiUrl = "{{ route('google-places.fetch') }}";
                 const spinner = $(
-                    '<div class="spinner-border mt-3" role="status"><span class="visually-hidden">Loading...</span></div>'
+                    '<div class="spinner-border mt-3" role="status"> <span class="visually-hidden">Loading...</span></div>'
                 );
+                const apiUrl = "{{ route('google-places.fetch') }}";
+
                 $("#placesCheckboxes").empty().append(spinner);
-                const hiddenInput = $('<input>').attr({
-                    type: 'hidden',
-                    name: 'placeId', // Set the name attribute
-                    value: placeKeyWord // Set the value to the selected category
+                const hiddenInput = $("<input>").attr({
+                    type: "hidden",
+                    name: "placeId", // Set the name attribute
+                    value: placeKeyWord, // Set the value to the selected category
                 });
 
                 // Append the hidden input to the form
-                $('form').append(hiddenInput);
+                $("form").append(hiddenInput);
                 $.ajax({
                     url: apiUrl,
                     method: "GET",
                     data: {
-                        category: placeKeyWord
+                        category: placeKeyWord,
                     },
                     dataType: "json",
                     success: function(response) {
@@ -110,19 +132,20 @@
                             const checkbox = $("<input>")
                                 .attr("type", "checkbox")
                                 .attr("name", "branches[]") // Use the same name for all checkboxes
-                                .attr("value", place['place_id'])
+                                .attr("value", place["place_id"])
                                 .addClass("mx-2 p-2")
                                 .attr("id", "place_" + index) // Use a unique ID for each checkbox
                                 .css({
                                     width: "20px",
-                                    height: "20px"
+                                    height: "20px",
                                 });
                             const label = $("<label>")
                                 .attr("for", "place_" + index)
                                 .text(place.name);
 
-                            const checkboxContainer = $("<div class='d-flex align-items-center'>")
-
+                            const checkboxContainer = $(
+                                    "<div class='d-flex align-items-center'>"
+                                )
                                 .append(checkbox)
                                 .append(label);
 
@@ -137,20 +160,19 @@
                     complete: function() {
                         // Hide the loading spinner
                         $("#placesCheckboxes").find(".spinner-border").remove();
-                    }
+                    },
                 });
             }
         }
         setTimeout(function() {
-            $('#successMessage').fadeOut('slow');
+            $("#successMessage").fadeOut("slow");
         }, 1000);
 
         // Function to hide error message after 1 second
         setTimeout(function() {
-            $('#errorMessage').fadeOut('slow');
+            $("#errorMessage").fadeOut("slow");
         }, 2000);
     </script>
-
 </body>
 
 </html>
